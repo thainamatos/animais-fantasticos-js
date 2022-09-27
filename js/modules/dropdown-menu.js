2b9357c175d8acf3272b68850ce20a3ce46b6a8f
Thainá Matos <thainamatos.if@hotmail.com>
2022-09-27 17:08:07 -0300

    ajustando medias e tooltip

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,3 +1,19 @@
+import outsideClick from './outsideclick.js';
+
 export default function initDropdownMenu() {
-    
+    const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+
+    dropdownMenus.forEach(menu => {
+        ['click', 'touchstart'].forEach(userEvent => {
+            menu.addEventListener(userEvent, handleClick);
+        })
+    })
+
+    function handleClick(event) {
+        event.preventDefault();
+        this.classList.add('active');
+        outsideClick(this, () => {
+            this.classList.remove('active');
+        });
+    }
 }
--- js/modules/tooltip.js
+++ js/modules/tooltip.js
@@ -1,6 +1,6 @@
 export default function initTooltip() {
     const tooltips = document.querySelectorAll('[data-tooltip]');
-    console.log('[data-tooltip]');
+    
   
     tooltips.forEach((item) => {
       item.addEventListener('mouseover', onMouseOver);
